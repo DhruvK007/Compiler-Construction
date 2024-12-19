2fad6644dc1fd5c0cad8140db584e2ada3cf93d4
Dhruv Kotadiya <22ce054@charusat.edu.in>
2024-12-19 12:36:08 +0530

    first commit

---
+++ Practical1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(){
+    char str[100];
+    printf("Enter a string: ");
+    scanf("%s",str);
+    for (int i = 0; i < strlen(str)-2; i++){
+        if(str[i] != 'a'){
+            printf("Invalid String");
+            return 0;
+        }
+    }
+
+    if(str[strlen(str)-2] == 'b' && str[strlen(str)-1] == 'b'){
+        printf("Valid String");
+    }else{
+        printf("Invalid String");
+    }
+
+    return 0;
+}
